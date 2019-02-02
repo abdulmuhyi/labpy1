b5b30e17d0ee68a1e5b198498f394ddfd3421920
abdulmuhyi <43901361+abdulmuhyi@users.noreply.github.com>
2019-02-02 12:10:08 +0700

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,14 @@
+a = int(input ("masukan nilai a : "))
+b = int(input ("masukan nilai b : "))
+c = int(input ("masukan nilai c : "))
+
+if a > b :
+    if a > c :
+        print ("nilai terbesarnya adalah", a)
+    else :
+        print ("nilai terbesarnya adalah", c)
+    else b > c :
+        print ("nilai terbesarnya adalah", b)
+    else :
+        print ("nilai terbesarnya adalah", c)
+            
